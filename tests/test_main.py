5176134fb0ddd98d736e879387092427782a49ed
Roharui <jack555586@gmail.com>
2022-08-13 19:51:38 +0900

    test: 테스트용 커밋
    테스트용 커밋입니다. merge되지 않습니다.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ from src.main import app
 
 client = TestClient(app)
 
-# 이 커밋은 테스트용 커밋입니다.
+# 이 커밋은 테스트용 커밋입니다. #
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
