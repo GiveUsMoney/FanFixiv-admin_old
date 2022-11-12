64f9ae1052067f7f63e80293a37423ef2ecadcb2
jaeha <tkdgkwogk@gmail.com>
2022-11-12 19:00:49 +0900

    feat : github action test 실행
    github action으로 pytest 할 수 있게 환경을 구성했습니다.
    test 용 db dependency도 추가했습니다

--- src/database.py
+++ src/database.py
@@ -8,8 +8,10 @@ import sqlalchemy
 import os
 
 # load_dotenv()
-
-sqlalchemy_database_url = os.environ["DB_URL"]
+if os.environ.get("DB_URL") is None:
+    sqlalchemy_database_url = "sqlite:///./tests/test.db"
+else:
+    sqlalchemy_database_url = os.environ["DB_URL"]
 # sqlalchemy_database_url = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["POSTGRES_HOST"]}/postgres'
 # sqlalchemy_database_url = f'postgresql://{os.getenv("db_user")}:{os.getenv("db_password")}@{os.getenv("db_host")}/{os.getenv("db_name")}'
 
